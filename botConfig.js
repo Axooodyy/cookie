c259bbb30f610615f45723e29af8adbf8c627549
Axooodyy <79177772+Axooodyy@users.noreply.github.com>
2021-03-21 17:44:01 +0300

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "ODIyMTQ0NTU4Nzc0MjIyODgw.YFN_yQ.5M2kvHRF8-ceUD29WLYco1KRAzMTOKEN",
+    prefix: "c?",
+    admins: [
+        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+],
+    debug: true,
+    countChannel: "countChannelID"
+};
